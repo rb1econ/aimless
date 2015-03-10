ec31c0c2e712f0c24f760d4516b7a5c923b3814b
Ryan budny <rb1econ@gmail.com>
2015-03-10 17:57:04 -0600

    first

---
+++ public/javascripts/main.js
@@ -0,0 +1,120 @@
+var map;
+var directionsDisplay;
+var directionsService;
+var stepDisplay;
+var markerArray = [];
+var handleNoGeolocation;
+var pos;
+
+function initialize() {
+  // Instantiate a directions service.
+  directionsService = new google.maps.DirectionsService();
+
+  // Create a map and center it on Manhattan.
+  var manhattan = new google.maps.LatLng(40.7711329, -73.9741874);
+  
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Location found using HTML5. Accuracy = '+position.coords.accuracy+'m'
+      });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
+
+  var mapOptions = {
+    zoom: 13,
+    center: pos
+  }
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  // Create a renderer for directions and bind it to the map.
+  var rendererOptions = {
+    map: map
+  }
+  directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions)
+
+  // Instantiate an info window to hold step text.
+  stepDisplay = new google.maps.InfoWindow();
+}
+
+function calcRoute() {
+
+  // First, remove any existing markers from the map.
+  for (var i = 0; i < markerArray.length; i++) {
+    markerArray[i].setMap(null);
+  }
+
+  // Now, clear the array itself.
+  markerArray = [];
+
+  // Retrieve the start and end locations and create
+  // a DirectionsRequest using WALKING directions.
+  // var start = document.getElementById('start').value;
+  start = pos;
+  var end = document.getElementById('end').value;
+  var request = {
+      origin: start,
+      destination: end,
+      travelMode: google.maps.TravelMode.WALKING
+  };
+
+  // Route the directions and pass the response to a
+  // function to create markers for each step.
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      var warnings = document.getElementById('warnings_panel');
+      warnings.innerHTML = '<b>' + response.routes[0].warnings + '</b>';
+      directionsDisplay.setDirections(response);
+      showSteps(response);
+    }
+  });
+}
+
+function showSteps(directionResult) {
+  // For each step, place a marker, and add the text to the marker's
+  // info window. Also attach the marker to an array so we
+  // can keep track of it and remove it when calculating new
+  // routes.
+  var myRoute = directionResult.routes[0].legs[0];
+
+  for (var i = 0; i < myRoute.steps.length; i++) {
+    var marker = new google.maps.Marker({
+      position: myRoute.steps[i].start_location,
+      map: map
+    });
+    attachInstructionText(marker, myRoute.steps[i].instructions);
+    markerArray[i] = marker;
+  }
+}
+
+function attachInstructionText(marker, text) {
+  google.maps.event.addListener(marker, 'click', function() {
+    // Open an info window when the marker is clicked on,
+    // containing the text of the step.
+    stepDisplay.setContent(text);
+    stepDisplay.open(map, marker);
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+// adding button and check box SHTUFF:::::::::::
+
+$('#iAmWalking').on('click', function(e){
+  e.preventDefault();
+  if($('#newDest:checked').val()){
+    // display input box person puts address into.
+  }
+});
