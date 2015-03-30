57a718f8819efb29c232ad6a0067504aba6e9eba
rb1econ <rb1econ@gmail.com>
2015-03-30 16:38:20 -0600

    down

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 
-mongoose.connect('mongodb://localhost/aimless_test');
+// mongoose.connect('mongodb://localhost/aimless_test');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
