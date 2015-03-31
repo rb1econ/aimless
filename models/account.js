d54600e21849986a7ab674be37be91c3593d7ace
rb1econ <rb1econ@gmail.com>
2015-03-30 19:13:50 -0600

    working

--- models/account.js
+++ models/account.js
@@ -1,11 +1,25 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
+var uriUtil = require('mongodb-uri');
 
-var Account = new Schema({
-    username: String,
-    password: String
-});
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };  
+
+var mongodbUri = 'mongodb://user:pass@host:port/db';
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+
+mongoose.connect(mongooseUri, options);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function callback () {
+	var Account = new Schema({
+	    username: String,
+	    password: String
+	});
 
 Account.plugin(passportLocalMongoose);
 
